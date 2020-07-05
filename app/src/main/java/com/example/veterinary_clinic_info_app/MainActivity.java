106b2b260355dbf3ec5a4050028e7681fa85c6fc
erika <one.step.closer.0x0@gmail.com>
2020-07-05 15:40:58 +0900

    add layout file etc...

--- app/src/main/java/com/example/veterinary_clinic_info_app/MainActivity.java
+++ app/src/main/java/com/example/veterinary_clinic_info_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.veterinary_clinic_info_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ButtonChat = findViewById(R.id.button_chat);
+        ButtonChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button ButtonCall = findViewById(R.id.button_call);
+        ButtonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
