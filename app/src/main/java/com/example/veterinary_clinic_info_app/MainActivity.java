d6cf8e5bccb211eb7af9cf612a4c76507c5f1956
erika <one.step.closer.0x0@gmail.com>
2020-07-06 17:56:14 +0900

    add json parse and button visible

--- app/src/main/java/com/example/veterinary_clinic_info_app/MainActivity.java
+++ app/src/main/java/com/example/veterinary_clinic_info_app/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -22,8 +25,13 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AsyncHttpRequest asyncHttpRequest;
-    private ProgressBar progressBar;
+    private ProgressBar progressBarConfig;
+    private ProgressBar progressBarPets;
+    private Button buttonChat;
+    private Button buttonCall;
+    private TextView textView;
+
+    final int RESPONSE_CODE_SUCCESS = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +39,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("TecnicTest", "onCreate");
-        ((TextView)findViewById(R.id.text_info)).setText(String.format(getString(R.string.office_hours),"----"));
+        textView = findViewById(R.id.text_info);
+        setWorkhours("----");
 
-        Button ButtonChat = findViewById(R.id.button_chat);
-        ButtonChat.setOnClickListener(v -> {
+        buttonChat = findViewById(R.id.button_chat);
+        buttonChat.setOnClickListener(v -> {
             //ここに押下時の処理
         });
 
-        Button ButtonCall = findViewById(R.id.button_call);
-        ButtonCall.setOnClickListener(v -> {
+        buttonCall = findViewById(R.id.button_call);
+        buttonCall.setOnClickListener(v -> {
             //ここに押下時の処理
         });
+
+        progressBarConfig = findViewById(R.id.progressBarHorizontalConfig);
+        progressBarPets = findViewById(R.id.progressBarHorizontalPets);
+
 
         fetchConfig();
         fetchPets();
@@ -49,10 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fetchConfig() {
-        //        asyncHttpRequest = new AsyncHttpRequest(this);
-//        asyncHttpRequest.execute("http://www.amock.io/api/erika178/config.json");
-
-        showProgress(R.id.progressBarHorizontalConfig);
+        showProgress(progressBarConfig);
         OkHttpClient client = new OkHttpClient();
 
         Request requestConfig = new Request.Builder()
@@ -63,15 +73,60 @@ public class MainActivity extends AppCompatActivity {
             final Handler mainHandler = new Handler(Looper.getMainLooper());
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                Log.i("TecnicTest", "onResponse " +"responsecode => " + response.code() +"responsebody => " + response.body().string());
+
+            String responseBody = response.body().string();
+                Log.i("TecnicTest", "onResponse " + "responsecode => " + response.code() + "responsebody => " + responseBody);
                 //ここにパースとか
-                mainHandler.post(() -> hideProgress(R.id.progressBarHorizontalConfig));
+                if (response.code() == RESPONSE_CODE_SUCCESS) {
+                    try {
+                        JSONObject jsonRoot = new JSONObject(responseBody);
+                        JSONObject jsonSetting = (JSONObject) jsonRoot.get("settings");
+                        boolean isChatEnabled = jsonSetting.getBoolean("isChatEnabled");
+                        boolean isCallEnabled = jsonSetting.getBoolean("isCallEnabled");
+                        String workHours = jsonSetting.getString("workHours");
+
+                        Log.i("TecnicTest", "isChatEnabled " + isChatEnabled);
+                        Log.i("TecnicTest", "isCallEnabled " + isCallEnabled);
+                        mainHandler.post(() -> {
+                            setButtonVisible(buttonChat,isChatEnabled);
+                            setButtonVisible(buttonCall,isCallEnabled);
+                            setWorkhours(workHours);
+                        });
+
+                    } catch (JSONException je) {
+                        Log.i("TecnicTest", "Json parse error!");
+                    }
+
+
+                }else {
+//                    //エラー
+
+//                    //これだとアプリ落ちたかも
+//                    mainHandler.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+//                            builder.setMessage("Data get error!!")
+//                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                                        public void onClick(DialogInterface dialog, int id) {
+//                                            finish();
+//                                        }
+//                                    });
+//                            builder.show();
+//                        }
+//                    });
+
+                }
+
+
+
+                mainHandler.post(() -> hideProgress(progressBarConfig));
             }
 
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 Log.i("TecnicTest", "onFailure " + e.getMessage());
-                mainHandler.post(() -> hideProgress(R.id.progressBarHorizontalConfig));
+                mainHandler.post(() -> hideProgress(progressBarConfig));
             }
         });
 
@@ -79,10 +134,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fetchPets() {
-
-
-
-        showProgress(R.id.progressBarHorizontalPets);
+        showProgress(progressBarPets);
         OkHttpClient client = new OkHttpClient();
 
         Request requestPets = new Request.Builder()
@@ -95,33 +147,43 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 Log.i("TecnicTest", "onResponse " +"responsecode => " + response.code() +"responsebody => " + response.body().string());
                 //ここにパースとか
-                mainHandler.post(() -> hideProgress(R.id.progressBarHorizontalPets));
+                mainHandler.post(() -> hideProgress(progressBarConfig));
             }
 
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 Log.i("TecnicTest", "onFailure " + e.getMessage());
-                mainHandler.post(() -> hideProgress(R.id.progressBarHorizontalPets));
+                mainHandler.post(() -> hideProgress(progressBarConfig));
             }
         });
 
         Log.i("TecnicTest", "after enqueue");
     }
 
-    private void showProgress(int id) {
-        progressBar = findViewById(id);
+    private void showProgress(ProgressBar progressBar) {
         progressBar.setVisibility(ProgressBar.VISIBLE);
     }
 
-    private void hideProgress(int id) {
-        progressBar = findViewById(id);
+    private void hideProgress(ProgressBar progressBar) {
         progressBar.setVisibility(ProgressBar.INVISIBLE);
+    }
+
+    private void setButtonVisible(Button button, boolean visible) {
+        if (visible) {
+            button.setVisibility(View.VISIBLE);
+            button.setEnabled(true);
+        } else {
+            button.setVisibility(View.GONE);
+        }
+    }
+
+    private void setWorkhours(String workhours){
+        textView.setText(String.format(getString(R.string.office_hours),workhours));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-//        asyncHttpRequest.cancel(false);
         Log.i("TecnicTest", "onDestroy");
     }
 }
